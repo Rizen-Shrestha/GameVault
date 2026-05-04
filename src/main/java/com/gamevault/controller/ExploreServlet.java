24ec39ad4b188a924d2fe6d3abf805ceac4bcda2
Rizen <rizenshrestha6063@gmail.com>
2026-05-04 07:16:26 +0545

    Explore servlet fixed

--- src/main/java/com/gamevault/controller/ExploreServlet.java
+++ src/main/java/com/gamevault/controller/ExploreServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 /**
  * Servlet implementation class ExploreServlet
  */
-@WebServlet("/ExploreServlet")
+@WebServlet("/explore")
 public class ExploreServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
